f960ff0fd7ae81b5c3e73abcc258d487c16bf6b2
Nikhil Jain <blockerdude2@yahoo.com>
2018-07-10 00:39:50 -0500

    Grid is working correclty and with dynamic variables

--- src/app/grid/hexagon/hexagon.component.ts
+++ src/app/grid/hexagon/hexagon.component.ts
@@ -8,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
 export class HexagonComponent implements OnInit {
 
   imageString: string;
+  width: string;
+  height: string;
+  oddMarginLeft: string;
+  oddMarginTop: string;
 
   ngOnInit() {
     // this.imageString = 'url(\'./../../../../bird.jpg\')';
     this.imageString = 'url(/assets/bird.jpg)';
+    const size = 120;
+    this.width = '' + Math.sqrt(3) * size;
+    this.height = '' + size * 2;
+    this.oddMarginLeft = '' + (+this.width / 2);
+    this.oddMarginTop = '-' + (+this.height / 4);
   }
 
   clicked() {
--- src/app/grid/hexgrid/hexgrid.component.ts
+++ src/app/grid/hexgrid/hexgrid.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HexgridComponent implements OnInit {
 
-  constructor() { }
+  imageString: string;
+  width: string;
+  height: string;
+  oddMarginLeft: string;
+  oddMarginTop: string;
 
   ngOnInit() {
+    const size = 120;
+    this.width = '' + Math.sqrt(3) * size;
+    this.height = '' + size * 2;
+    this.oddMarginLeft = '' + (+this.width / 2);
+    this.oddMarginTop = '-' + (+this.height / 4);
   }
 
 }
