522cdb8a0c13fe5d8f4b63ab67272748f33632e0
Nikhil Jain <nikhiljainmn15@gmail.com>
2018-08-21 12:49:17 -0500

    Remove unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { GridModule } from './grid/grid.module';
-import { DragScrollModule } from 'ngx-drag-scroll';
 
 
 @NgModule({
